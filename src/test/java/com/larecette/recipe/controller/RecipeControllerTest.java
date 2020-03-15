494c098cbf1e25c9e07b5159ef4f5dfa9416752a
Ann Severyna <linxyann@gmail.com>
2020-03-15 19:07:18 +0200

    Update commit

--- src/test/java/com/larecette/recipe/controller/RecipeControllerTest.java
+++ src/test/java/com/larecette/recipe/controller/RecipeControllerTest.java
@@ -77,6 +77,7 @@ class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+
     }
 
     @Test
