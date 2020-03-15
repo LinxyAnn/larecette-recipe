f94d0cc4b187d66744df8625d6b4a9038275b323
Ann Severyna <linxyann@gmail.com>
2020-03-15 19:24:30 +0200

    Add TestClass to dev branch

--- src/main/java/com/larecette/recipe/TestClass.java
+++ src/main/java/com/larecette/recipe/TestClass.java
@@ -4,4 +4,8 @@ public class TestClass {
     /**
      * TestClass for dev branches
      */
+
+    public static void main(String[] args) {
+        System.out.println("Some text testadd");
+    }
 }
